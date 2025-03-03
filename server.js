998a1c3a97a347798c633aabc6f87c2fe8ac1567
Balram Shukla <balramshukla003@gmail.com>
2025-03-03 22:26:10 +0530

    upload

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
-app.use(cors({
-    origin: '*',
-    credentials: true,
-    methods: 'GET, POST, PUT, DELETE',
-}));
 dotenv.config();
 app.use(express.json());
 app.use(bodyParser.json());
