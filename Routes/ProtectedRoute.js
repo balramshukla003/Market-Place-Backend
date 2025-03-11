c7bbdbf40c710379c3ede448313c2a01014e33c8
Balram Shukla <balramshukla003@gmail.com>
2025-03-11 19:50:21 +0530

    update

--- Routes/ProtectedRoute.js
+++ Routes/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authenticateToken from '../verifyTKN.js';
+import authenticateToken from '../JWT_TKN.js';
 
 const router = express.Router();
 router.post('/', async (req, res, next) => {
